dc74a876409cd9ba6885d1fbc674e60caa162d3b
Anik <sheikanikbd@gmail.com>
2025-08-04 22:45:41 +0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const prisma = new PrismaClient();
 
 // ✅ Enable CORS for frontend origin
 app.use(cors({
-  origin: '*', // your Next.js frontend URL
+  origin: process.env.FRONTEND_URL, // your Next.js frontend URL
   credentials: true,              // if using cookies or authorization headers
 })); 
 
